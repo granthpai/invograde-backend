cdedc5d20d7d37534ac936d556a19d74bc209940
granthpai <rockingranth@gmail.com>
2025-06-05 20:16:09 +0530

    fixed auth

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,9 @@
-declare namespace Express {
+import { IUser } from '../models/user';
+
+declare global {
+  namespace Express {
     interface Request {
-      user: {
-        id: string;
-        email?: string;
-      };
+      user?: IUser;
     }
   }
-  
+}
