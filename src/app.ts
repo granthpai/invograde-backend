29167d969cdfa6f55cb58861087e6118c2139aa6
Jatin Chaudhary <09jatin2005@gmail.com>
2025-06-26 22:42:31 +0530

    domain added

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = process.env.FRONTEND_URLS?.split(",") || [];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
